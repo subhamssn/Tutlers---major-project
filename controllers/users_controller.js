d87fd6bc860b20a69f517b9c33e18eba9cd0a57b
Shubham Singh <subhamsinghpg@gmail.com>
2023-04-17 18:07:49 +0530

    sign in & sign-up pages created

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,7 @@
 module.exports.sign_up = function(req, res){
     return res.render('user-sign-up');
 }
+
+module.exports.sign_in = function(req, res){
+    return res.render('user-sign-in');
+}
--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,8 @@ const router = express.Router();
 
 const usersController = require('../controllers/users_controller');
 
+
 router.get('/sign-up', usersController.sign_up);
+router.get('/sign-in', usersController.sign_in);
 
 module.exports = router;
