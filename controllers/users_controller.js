02f54164c039a17751ddf859c2299ee86951b990
Shubham Singh <subhamsinghpg@gmail.com>
2023-05-01 09:34:49 +0530

    before adding passport.js

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,5 @@
+const User = require('../models/user');
+
 module.exports.sign_up = function(req, res){
     return res.render('user-sign-up');
 }
@@ -6,11 +8,27 @@ module.exports.sign_in = function(req, res){
     return res.render('user-sign-in');
 }
 
-module.exports.create = function(req, res){
-
-      res.send('<h1>Sign-Up Post</h1>');
-
-}
+module.exports.create = async function(req, res) {
+    try {
+      if (
+        req.body.password != req.body.confirm-password
+      ) {
+        return res.redirect('back');
+      }
+  
+      const user = await User.findOne({ email: req.body.email }).exec();
+  
+      if (!user) {
+        const newUser = await User.create(req.body);
+        return res.redirect('/users/sign-in');
+      } else {
+        return res.redirect('back');
+      }
+    } catch (err) {
+      console.log('error in creating user while signing up', err);
+      return res.redirect('back');
+    }
+  };
 
 module.exports.createSession = function(req, res){
     res.send('<h1>Sign-In Post</h1>');
